b1054d352a57efd5784f32173203bb3665017783
yordanov0502 <yordanovtodor281@gmail.com>
2023-09-08 17:05:44 +0300

    Initial commit

---
+++ backend/src/test/java/bg/tu_varna/sit/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
