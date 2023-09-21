531f3a03e9d9fe1e90288b8787ee8298ae49c2b5
yordanov0502 <yordanovtodor281@gmail.com>
2023-09-21 23:26:07 +0300

    Unnecessary lines of code removed

--- backend/src/main/java/bg/tu_varna/sit/backend/exception/CustomExceptionHandler.java
+++ backend/src/main/java/bg/tu_varna/sit/backend/exception/CustomExceptionHandler.java
@@ -1,18 +1,8 @@
 package bg.tu_varna.sit.backend.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-
-    //!--------------------
-    /*@ExceptionHandler
-    public ProblemDetail handleException(BadCredentialsException badCredentialsException){
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, badCredentialsException.getMessage());
-    }*/
 
 }
