01039002c6465b77f15b679f0e38c5f785c0471c
yordanov0502 <yordanovtodor281@gmail.com>
2023-09-25 11:56:12 +0300

    spring-boot-starter-cache dependency added

--- backend/src/main/java/bg/tu_varna/sit/backend/BackendApplication.java
+++ backend/src/main/java/bg/tu_varna/sit/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package bg.tu_varna.sit.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
