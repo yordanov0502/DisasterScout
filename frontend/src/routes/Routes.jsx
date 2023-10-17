d0c178ad12e5167874cea635463973378dfba10e
yordanov0502 <yordanovtodor281@gmail.com>
2023-10-17 15:46:22 +0300

    frontend project updated

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 
+
+
 export const Router = () =>{
     return(
        <Routes>
 
         
-        <Route path="/login" element={<Login/>}> </Route>
+        <Route path="/login" element={<Login/>} />
        </Routes>
     );
 };
