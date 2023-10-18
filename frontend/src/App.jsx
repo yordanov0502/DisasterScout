3ed59d2a6a7a8016961e00049a1e069b1e2870c8
yordanov0502 <yordanovtodor281@gmail.com>
2023-10-18 17:13:57 +0300

    exports in frontend project updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Router } from "./routes/Routes"
+import { Router } from "./routes"
 
 
 function App() {
--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import { Login } from "../pages/Login";
+import { Login 
+} from "../pages";
 
 
 
