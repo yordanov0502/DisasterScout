3ee60ecfa2e54321fb0e4f50e433a5d80dddf6b5
yordanov0502 <yordanovtodor281@gmail.com>
2023-10-29 22:42:44 +0200

    Login component created

---
+++ frontend/src/components/Proba/Proba.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./proba.scss";
+
+export const Proba =()=>{
+  return (
+    <div className="container">
+    <div className="login wrap">
+      <div className="h1">Вход</div>
+      <form /*onSubmit={handleLogin}*/>
+      <input 
+        //pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" 
+        placeholder="Потребителско име" 
+        id="username" 
+        name="username" 
+        type="text"
+        autoComplete="current-username" //?????????????????????????????????????
+      />
+      <input 
+        placeholder="Парола" 
+        id="password" 
+        name="password" 
+        type="password"
+        autoComplete="current-password" //??????????????????????????????????
+      />
+      <input 
+        value="Login" 
+        className="btn" 
+        type="submit"
+      />
+      </form>
+    </div>
+    </div>
+  );
+};
--- frontend/src/pages/external/Login/Login.jsx
+++ frontend/src/pages/external/Login/Login.jsx
@@ -1,11 +1,13 @@
-import "./login.scss"
+import "./login.scss";
+//import {LoginC as LoginComponent} from "../../../components";
+import {Proba} from "../../../components";
 
 export const Login = () => {
 
 
     return(
       <div className="page_login">
-        { }
+        { <Proba/>}
       </div>
     );
 };
