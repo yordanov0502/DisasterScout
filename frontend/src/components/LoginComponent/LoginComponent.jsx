0dc27f350acb2df44afe55c90a442115432ecd10
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-04 20:58:30 +0200

    login component updated

--- frontend/src/components/LoginComponent/LoginComponent.jsx
+++ frontend/src/components/LoginComponent/LoginComponent.jsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./login_component.scss";
-
-const handleLogin = (event) => {
-  event.preventDefault(); // Prevent the default form submit action if using a submit button
-  console.log('Button clicked!');
-  // You can add more logic here for what happens when the button is clicked
-};
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
 
 export const LoginComponent = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('Button clicked!');
+  };
+
   return (
     <div className="component">
-    <div className="login">
-      <div className="img"></div>
-      <form onSubmit={handleLogin}>
-      <input 
-        //pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" 
-        id="username" 
-        name="username" 
-        type="text"
-        placeholder="Потребителско име" 
-        autoComplete="current-username" //?????????????????????????????????????
-      />
-      <input 
-        id="password" 
-        name="password" 
-        type="password"
-        placeholder="Парола" 
-        autoComplete="current-password" //??????????????????????????????????
-      />
-      <input 
-        value="Вход"        
-        type="submit"
-      />
-      </form>
-    </div>
+      <div className="login">
+        <div className="img"></div>
+        <form onSubmit={handleLogin}>
+          <div className="username-field">
+            <PersonIcon className={`username-icon ${username && 'active'}`}/>
+            <input 
+              id="username" 
+              name="username" 
+              type="text"
+              placeholder="Потребителско име"
+              autoComplete="username" //???
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="password-field">
+            <LockIcon className={`password-icon ${password && 'active'}`}/>
+            <input 
+              id="password" 
+              name="password" 
+              type="password"
+              placeholder="Парола"
+              autoComplete="current-password" //???
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <a href="#reset-password" className="forgot-password">Забравена парола?</a>
+          <input value="Вход" type="submit"/>
+        </form>
+      </div>
     </div>
   );
 };
