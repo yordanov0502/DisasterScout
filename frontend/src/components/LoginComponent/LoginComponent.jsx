250064dee4900e1c87f28f51da95351faf2e087a
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-04 18:36:37 +0200

    login component scss file modified

--- frontend/src/components/LoginComponent/LoginComponent.jsx
+++ frontend/src/components/LoginComponent/LoginComponent.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import "./login_component.scss";
 
+const handleLogin = (event) => {
+  event.preventDefault(); // Prevent the default form submit action if using a submit button
+  console.log('Button clicked!');
+  // You can add more logic here for what happens when the button is clicked
+};
+
 export const LoginComponent = () => {
   return (
     <div className="component">
     <div className="login">
-      <div className="h1"></div>
-      <form /*onSubmit={handleLogin}*/>
+      <div className="img"></div>
+      <form onSubmit={handleLogin}>
       <input 
         //pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" 
-        placeholder="Потребителско име" 
         id="username" 
         name="username" 
         type="text"
+        placeholder="Потребителско име" 
         autoComplete="current-username" //?????????????????????????????????????
       />
       <input 
-        placeholder="Парола" 
         id="password" 
         name="password" 
         type="password"
+        placeholder="Парола" 
         autoComplete="current-password" //??????????????????????????????????
       />
       <input 
-        value="Вход" 
-        className="button" 
-        //onClick={"dada" }
+        value="Вход"        
         type="submit"
       />
       </form>
