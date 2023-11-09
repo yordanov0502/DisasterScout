cb01b7df8a9944f1be87e7a5f3f2a7241b103f07
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-09 10:58:40 +0200

    Error message added to login component

--- frontend/src/components/LoginComponent/LoginComponent.jsx
+++ frontend/src/components/LoginComponent/LoginComponent.jsx
@@ -1,58 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./login_component.scss";
-import PersonIcon from '@mui/icons-material/Person';
-import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
 
 export const LoginComponent = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  //? pri natisnat button enter
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
+    setErrorMessage(""); // Clear error message when user starts typing
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    setErrorMessage(""); // Clear error message when user starts typing
   };
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    console.log('Button clicked!');
+  // Assuming you have some function to validate credentials
+  // You should replace this with your actual validation logic
+  const validateCredentials = (username, password) => {
+    // Replace with actual validation
+    return username === "validUser" && password === "validPass";
   };
+
+  const onPressLogin = (event) => {
+    event.preventDefault(); //???
+    //....................
+    // Assuming you have a validation function or method to check credentials
+    // If credentials are invalid set the error message
+    if (!username || !password) {
+      setErrorMessage("Моля въведете данни в полетата.");
+    } else if (!validateCredentials(username, password)) {
+      setErrorMessage("Невалидно потребителско име или парола.");
+    } else {
+      // Proceed with your login logic
+    }
+  };
+
   //* Невалидно потребителско име или парола.
   //* Моля въведете данни в полетата.
-       //!regex proverkite za username v JS ne poddurjat predvaritelni proverki dali string moje da zapochva ili svurshva s daden symbol
+  //!regex proverkite za username v JS ne poddurjat predvaritelni proverki dali string moje da zapochva ili svurshva s daden symbol
+
   return (
     <div className="component">
       <div className="login">
         <div className="img"></div>
-        <form onSubmit={handleLogin}>
+        <form onSubmit={onPressLogin}>
           <div className="username-field">
-            <PersonIcon className={`username-icon ${username && 'active'}`}/>
-            <input 
-              id="username" 
-              name="username" 
+            <PersonIcon className={`username-icon ${username && "active"}`} />
+            <input
+              id="username"
+              name="username"
               type="text"
               placeholder="Потребителско име"
-              autoComplete="username" //???
+              autoComplete="username"
               value={username}
               onChange={handleUsernameChange}
+              className={errorMessage ? "input-error" : ""}
             />
           </div>
           <div className="password-field">
-            <LockIcon className={`password-icon ${password && 'active'}`}/>
-            <input 
-              id="password" 
-              name="password" 
+            <LockIcon className={`password-icon ${password && "active"}`} />
+            <input
+              id="password"
+              name="password"
               type="password"
               placeholder="Парола"
-              autoComplete="current-password" //???
+              autoComplete="current-password"
               value={password}
               onChange={handlePasswordChange}
+              className={errorMessage ? "input-error" : ""}
             />
           </div>
-          <a href="/reset-password" className="forgot-password">Забравена парола?</a>
-          <input value="Вход" type="submit"/>
+          <a href="/reset-password" className="forgot-password">
+            Забравена парола?
+          </a>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <input value="Вход" type="submit" />
         </form>
       </div>
     </div>
--- frontend/src/components/ResetPasswordComponent/ResetPasswordComponent.jsx
+++ frontend/src/components/ResetPasswordComponent/ResetPasswordComponent.jsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import "./reset_password_component.scss";
 import EmailIcon from '@mui/icons-material/Email';
 
-export const ResetPasswordComponent = () => {
+export const ResetPasswordComponent = () => { //? pri natisnat button enter
   const [email, setEmail] = useState('');
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
-  const handleResetPassword = (event) => {
-    event.preventDefault();
-    console.log('Button clicked!');
+  const onPressResetPassword = (event) => {
+    event.preventDefault(); //???
+    //purvo validira prazni poleta/regex
+    //posle validira dali ima potrbitel s takiva danni
   };
        //!regex proverkite za username v JS ne poddurjat predvaritelni proverki dali string moje da zapochva ili svurshva s daden symbol
   return (
     <div className="component">
       <div className="reset-password">
       <div className="img"></div>
-        <form onSubmit={handleResetPassword}>
-             {/* Add a text block here */}
+        <form onSubmit={onPressResetPassword}>
         <div className="text-between">
            Моля въведете вашия имейл адрес. 
            Ще получите имейл с нова парола и инструкции. 
@@ -32,7 +32,7 @@ export const ResetPasswordComponent = () => {
               name="email" 
               type="email"
               placeholder="Имейл адрес"
-              //autoComplete="username" //???
+              //autoComplete="email" //???
               value={email}
               onChange={handleEmailChange}
             />
