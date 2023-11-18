e9bdcab419e3af8bc6693b9d76f521c19a62f8ec
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-18 14:22:32 +0200

    reset password logic extracted to the related page

--- frontend/src/components/ResetPasswordComponent/ResetPasswordComponent.jsx
+++ frontend/src/components/ResetPasswordComponent/ResetPasswordComponent.jsx
@@ -1,27 +1,7 @@
-import { useState } from "react";
 import "./reset_password_component.scss";
 import EmailIcon from "@mui/icons-material/Email";
-import { validateResetPasswordForm } from "../../validations";
 
-export const ResetPasswordComponent = () => {
-  const [email, setEmail] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleInput = (e) => {
-    setEmail(e.target.value.trim());
-    setErrorMessage(""); // Clear error message when user starts typing
-  };
-
-  const onPressResetPassword = (event) => {
-    event.preventDefault();
-
-    setErrorMessage(validateResetPasswordForm(email));
-    !errorMessage &&
-      {
-        // Proceed with your login logic
-        //? send email with newly generated password and instructions to the user with the provided email existing in DB
-      };
-  };
+export const ResetPasswordComponent = ({ email, errorMessage, handleInput, onPressResetPassword }) => {
 
   return (
     <div className="reset_password">
--- frontend/src/pages/external/ResetPasswordPage/ResetPasswordPage.jsx
+++ frontend/src/pages/external/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import "./reset_password_page.scss";
+import { validateResetPasswordForm } from "../../../validations";
 import {ResetPasswordComponent} from "../../../components";
 
 export const ResetPasswordPage = () => {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const handleInput = (e) => {
+    setEmail(e.target.value.trim());
+    setErrorMessage(""); // Clear error message when user starts typing
+  };
+
+  const onPressResetPassword = (event) => {
+    event.preventDefault();
+
+    setErrorMessage(validateResetPasswordForm(email));
+    !errorMessage &&
+      {
+        // Proceed with your login logic
+        //? send email with newly generated password and instructions to the user with the provided email existing in DB
+      };
+  };
 
     return(
       <div className="reset_password_page">
-        { <ResetPasswordComponent/>}
+        { <ResetPasswordComponent
+          email={email} 
+          errorMessage={errorMessage} 
+          handleInput={handleInput} 
+          onPressResetPassword={onPressResetPassword} 
+          />}
       </div>
     );
 };
