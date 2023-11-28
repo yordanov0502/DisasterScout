7126e47bcbe541d3d9e8dc25c5eb6aedb68f7f2a
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-29 00:09:34 +0200

    HTTPS enabled on API and security and application.properties configured

--- frontend/src/pages/external/LoginPage/LoginPage.jsx
+++ frontend/src/pages/external/LoginPage/LoginPage.jsx
@@ -17,31 +17,24 @@ export const LoginPage = () => {
     setErrorMessage(""); // Clear error message when user starts typing
   };
 
-
-  const API_URL = "http://localhost:8080/api/external/login";
+  const API_URL = "https://localhost/api/external/login";
   const [user, setUser] = useState({
     username: "ivanov50",
-    password: "B0502HTto$hko"
+    password: "B0502HTto$hko",
   });
 
   useEffect(() => {
-    const fetchData = async () =>{
-      
-      try{
-        const response = await axios.post(API_URL,user);
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(API_URL, user);
         setUser(response.data);
-     }
-     catch(error){
-         console.log(error);
-     }
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     fetchData();
-  },[]);
-
-  
-
-
+  }, []);
 
   const onPressLogin = (event) => {
     event.preventDefault();
