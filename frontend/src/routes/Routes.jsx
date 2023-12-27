d7f27b5b33b9c9ec50b7cdb6f4e9c799b68b1be2
yordanov0502 <yordanovtodor281@gmail.com>
2023-12-27 13:19:23 +0200

    Routes file updated.

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -4,18 +4,35 @@ import { lazy } from "react";
 //* Pages are loaded lazily for successfull implementation of code splitting
 //! React.lazy can work ONLY with default exports
 //? Each lazy import correctly targets a named export from a module and wraps it as a default export, so that React.lazy can work properly
-const LoginPage = lazy(()=> import("../pages/external/LoginPage").then((module)=>({default: module.LoginPage})))
-const ResetPasswordPage = lazy(()=> import("../pages/external/ResetPasswordPage").then((module)=>({default: module.ResetPasswordPage})))
-const WelcomePage = lazy(()=> import("../pages/external/WelcomePage").then((module)=>({default: module.WelcomePage})))
+const LoginPage = lazy(() =>
+  import("../pages/external/LoginPage").then((module) => ({
+    default: module.LoginPage,
+  }))
+);
+const ResetPasswordPage = lazy(() =>
+  import("../pages/external/ResetPasswordPage").then((module) => ({
+    default: module.ResetPasswordPage,
+  }))
+);
+const WelcomePage = lazy(() =>
+  import("../pages/external/WelcomePage").then((module) => ({
+    default: module.WelcomePage,
+  }))
+);
+const NotFoundPage = lazy(() =>
+  import("../pages/external/NotFoundPage").then((module) => ({
+    default: module.NotFoundPage,
+  }))
+);
 
 export const Router = () => {
   return (
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-        <Route index element={<WelcomePage />} />
-        {/* <Route path="*" element={<NoPage />} /> */}
-      </Routes>
+      <Route index element={<WelcomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
