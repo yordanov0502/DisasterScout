36263c100fa5d2e2ed191b855284f8882b8f1558
yordanov0502 <yordanovtodor281@gmail.com>
2024-01-14 00:01:56 +0200

    Button on NotFound page updated

--- frontend/src/components/NotFoundComponent/NotFoundComponent.jsx
+++ frontend/src/components/NotFoundComponent/NotFoundComponent.jsx
@@ -1,12 +1,21 @@
 import { Button } from "@mui/material";
 import "./not_found_component.scss";
+import { useNavigate } from "react-router-dom";
 
 export const NotFoundComponent = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="not_found_component">
       Изглежда, че се отклонихте от маршрута. Страницата, която търсите не
       съществува или в момента не е налична.
-        <Button className="not_found_component__button">Начало</Button>
+      <Button className="not_found_component__button" onClick={handleClick}>
+        Начало
+      </Button>
     </div>
   );
 };
