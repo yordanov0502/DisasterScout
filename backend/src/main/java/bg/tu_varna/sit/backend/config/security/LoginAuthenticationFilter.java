d7efae636f42c5cce44366d444a545efaa86fc1b
yordanov0502 <yordanovtodor281@gmail.com>
2024-02-14 16:58:33 +0200

    LoginAuthenticationFilter updated

--- backend/src/main/java/bg/tu_varna/sit/backend/config/security/LoginAuthenticationFilter.java
+++ backend/src/main/java/bg/tu_varna/sit/backend/config/security/LoginAuthenticationFilter.java
@@ -5,7 +5,6 @@ import bg.tu_varna.sit.backend.models.entity.User;
 import bg.tu_varna.sit.backend.service.UserService;
 import bg.tu_varna.sit.backend.validation.user.CustomLoginRegexValidation;
 import com.google.gson.Gson;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -63,18 +62,16 @@ public class LoginAuthenticationFilter {
             User principal = (User) authentication.getPrincipal();
             userService.updateUserActivityAndLastLogin(principal);
             String generatedJWT = jwtService.generateToken(userService.getUserById(principal.getId()));
-            //? cookie of type "ResponseCookie" is used instead of type "Cookie", because the "ResponseCookie" type has an attribute SameSite, which I can set for security reasons
-            ResponseCookie cookie = ResponseCookie.from(HTTP_ONLY_COOKIE_NAME,generatedJWT)
+            //? httpOnlyCookie of type "ResponseCookie" is used instead of type "Cookie", because the "ResponseCookie" type has an attribute SameSite, which I can set for security reasons
+            ResponseCookie httpOnlyCookie = ResponseCookie.from(HTTP_ONLY_COOKIE_NAME,generatedJWT)
                     .httpOnly(true)
-                    .secure(true) //? Indicates to the browser whether the cookie should only be sent using a secure protocol, such as HTTPS or SSL.
+                    .secure(true) //? Indicates to the browser whether the httpOnlyCookie should only be sent using a secure protocol, such as HTTPS or SSL.
                     .domain(BACKEND_DOMAIN)
-                    .path(BACKEND_SECURED_PATH) //! This cookie should be sent from the client to the backend only when this specific path is matched. All secured paths begin with this part.
-                    .maxAge(jwtService.getSecondsOfJwtValidity(principal)) //? Max age of the httpOnly cookie MUST be equal to the validity period of the JWT, just to make sure the httpOnly cookie is not deleted by the browser while the JWT is still valid (in normal conditions excluding the logout business logic). Both depending on the role of the user.
+                    .path(BACKEND_SECURED_PATH) //! This httpOnlyCookie should be sent from the client to the backend only when this specific path is matched. All secured paths begin with this part.
+                    .maxAge(jwtService.getSecondsOfJwtValidity(principal)) //? Max age of the httpOnlyCookie MUST be equal to the validity period of the JWT, just to make sure the httpOnlyCookie is not deleted by the browser while the JWT is still valid (in normal conditions excluding the logout business logic). Both depending on the role of the user.
                     .sameSite(SAME_SITE_POLICY1)
                     .build();
-            //!!! ANOTHER COOKIE TO SET FOR LOCAL STORAGE
-            httpServletResponse.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-            //!!! ANOTHER HEADER TO SET WITH THE COOKIE FOR THE LOCAL STORAGE
+            httpServletResponse.setHeader(HttpHeaders.SET_COOKIE, httpOnlyCookie.toString());
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
