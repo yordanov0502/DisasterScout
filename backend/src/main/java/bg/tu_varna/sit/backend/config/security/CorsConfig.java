b989b701a7b605f23480465aa70d3868ec4db97c
yordanov0502 <yordanovtodor281@gmail.com>
2024-03-11 13:39:52 +0200

    CORS policy updated

--- backend/src/main/java/bg/tu_varna/sit/backend/config/security/CorsConfig.java
+++ backend/src/main/java/bg/tu_varna/sit/backend/config/security/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
         //corsConfiguration.setAllowedOrigins(List.of("https://localhost")); //this also works, but the line below is preferred, because it written in the docs when you click over "setAllowedOriginPatterns" that it can work along with setAllowCredentials set to true
         corsConfiguration.setAllowedOriginPatterns(List.of("https://localhost")); //? allowedOriginPatterns can be used in combination with setAllowCredentials set to true.
         corsConfiguration.setAllowCredentials(true); //? allows cookies
-        corsConfiguration.setAllowedHeaders(List.of("*")); //!If you do not add the custom header to the CORS configuration, the browser will not expose it to the JavaScript on the client side, even though it might still be sent by the server and visible in the developer tools. When credentials are involved, wildcard characters like "*" do not work as expected. By default, only a few headers are exposed (Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma).
+        corsConfiguration.setAllowedHeaders(List.of("*")); //! The wildcard DOES NOT work! I must add all allowed headers(only those I need of course) here explicitly(manually) . By default  CORS-safelisted request headers are always allowed
         corsConfiguration.setAllowedMethods(List.of(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
@@ -41,7 +41,7 @@ public class CorsConfig {
                 HttpMethod.TRACE.name(),
                 HttpMethod.PATCH.name(),
                 "CONNECT"));
-        corsConfiguration.setExposedHeaders(List.of("*",REDIRECT_TO_LOGIN_PAGE_RESPONSE_HEADER)); //!!! Should I expose all headers on response ? or not to expose any (new ArrayList<>())? https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/cors/CorsConfiguration.html#setExposedHeaders(java.util.List)  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers
+        corsConfiguration.setExposedHeaders(List.of(/*"*",*/REDIRECT_TO_LOGIN_PAGE_RESPONSE_HEADER)); //! If I don't add the headers explicitly(manually) to the CORS configuration, the browser will not expose them to the JavaScript on the client side, even though they might still be sent by the server and visible in the developer tools. When credentials are involved, wildcard characters like "*" DO NOT work!!!
 
         //* Different browsers might have their own maximum limits for caching, and these limits could be much lower
         //* than the value you set. Exceeding these limits could result in the browser defaulting to a lower, more standard value.
