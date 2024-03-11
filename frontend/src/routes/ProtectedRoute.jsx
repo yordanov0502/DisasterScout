5f4631138ad4ab126f9c91068f4e98fa06617839
yordanov0502 <yordanovtodor281@gmail.com>
2024-03-11 23:46:47 +0200

    ProtectedRoute updated

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,12 +2,14 @@ import { Navigate } from "react-router-dom";
 import { useUserContext } from "../hooks/useUserContext";
 import { useQuery } from "@tanstack/react-query";
 import { isUserAuthenticated } from "../services/userService";
+import { useRef } from "react";
 
 const LOCAL_STORAGE_KEY1 = `${import.meta.env.VITE_LOCAL_STORAGE_KEY1}`;
 const LOCAL_STORAGE_VALUE1 = `${import.meta.env.VITE_LOCAL_STORAGE_VALUE1}`;
 
 export const ProtectedRoute = ({ children }) => {
   const { isUserContextEmpty,updateUserContext } = useUserContext();
+  const hasUpdatedUserContext = useRef(false);
 
   const getUserAuthentication = useQuery({
     queryKey: ["isUserAuthenticated"],
@@ -16,32 +18,35 @@ export const ProtectedRoute = ({ children }) => {
   });
 
   function doWork(data) {
-    updateUserContext(data);
-    console.log("updated user context");
+    if(!hasUpdatedUserContext.current){
+      updateUserContext(data);
+      hasUpdatedUserContext.current = true; // Set the flag to true after updating
+    }
+    //console.log("updated user context");
+    console.log(data);
     return children;
   }
 
-  if (
-    localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
-    localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
-    !isUserContextEmpty()
-  ) {console.log("case 1");
-    return children;
-   } 
-//    else if (
-//     localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
-//     localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
-//     isUserContextEmpty()
-//   ) {
-//     //getUserAuthentication.refetch();
-//     //console.log(getUserAuthentication.isSuccess);
-
-//     //return getUserAuthentication.isLoading && <h1>Loading...</h1>
-//     console.log("case 2");
-//      return (getUserAuthentication.isSuccess && doWork(getUserAuthentication.data));
-//   } 
-  else {
-    console.log("case 3");
-    return <Navigate to="/login" />;
-  }
+  return(
+    <>
+         {
+          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
+          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
+          !isUserContextEmpty() && children
+         }
+         {
+          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
+          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
+          isUserContextEmpty() && getUserAuthentication.isLoading && <h1>Loading</h1> 
+         }
+         {
+          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
+          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
+          isUserContextEmpty() && getUserAuthentication.isSuccess &&  doWork(getUserAuthentication.data)
+         }
+         {
+           localStorage.getItem(LOCAL_STORAGE_KEY1) === null && <Navigate to="/login" />
+         }
+    </>
+  );
 };
