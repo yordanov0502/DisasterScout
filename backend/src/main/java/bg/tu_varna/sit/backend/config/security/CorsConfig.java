c014d75a87b0ff97657de7981cd87c85688a189a
yordanov0502 <yordanovtodor281@gmail.com>
2024-03-11 12:48:42 +0200

    CORS policy updated

--- backend/src/main/java/bg/tu_varna/sit/backend/config/security/CorsConfig.java
+++ backend/src/main/java/bg/tu_varna/sit/backend/config/security/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
         //corsConfiguration.setAllowedOrigins(List.of("https://localhost")); //this also works, but the line below is preferred, because it written in the docs when you click over "setAllowedOriginPatterns" that it can work along with setAllowCredentials set to true
         corsConfiguration.setAllowedOriginPatterns(List.of("https://localhost")); //? allowedOriginPatterns can be used in combination with setAllowCredentials set to true.
         corsConfiguration.setAllowCredentials(true); //? allows cookies
-        corsConfiguration.setAllowedHeaders(List.of("*",REDIRECT_TO_LOGIN_PAGE_RESPONSE_HEADER)); //!If you do not add the custom header to the CORS configuration, the browser will not expose it to the JavaScript on the client side, even though it might still be sent by the server and visible in the developer tools. When credentials are involved, wildcard characters like "*" do not work as expected. By default, only a few headers are exposed (Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma).
+        corsConfiguration.setAllowedHeaders(List.of("*")); //!If you do not add the custom header to the CORS configuration, the browser will not expose it to the JavaScript on the client side, even though it might still be sent by the server and visible in the developer tools. When credentials are involved, wildcard characters like "*" do not work as expected. By default, only a few headers are exposed (Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma).
         corsConfiguration.setAllowedMethods(List.of(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
