3d4f1fcccc31a7aa4f564a4c1f7ba3c64a5ee4ba
yordanov0502 <yordanovtodor281@gmail.com>
2024-03-11 10:47:22 +0200

    Lazy loading of pages removed due to multiple rendering of App and Router components

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,36 +1,9 @@
-import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-
-//* Pages are loaded lazily for successfull implementation of code splitting
-//! React.lazy can work ONLY with default exports
-//? Each lazy import correctly targets a named export from a module and wraps it as a default export, so that React.lazy can work properly
-const LoginPage = lazy(() =>
-  import("../pages/external/LoginPage").then((module) => ({
-    default: module.LoginPage,
-  }))
-);
-const ForgotPasswordPage = lazy(() =>
-  import("../pages/external/ForgotPasswordPage").then((module) => ({
-    default: module.ForgotPasswordPage,
-  }))
-);
-
-const CmsHomePage = lazy(() =>
-  import("../pages/internal/CmsHomePage").then((module) => ({
-    default: module.CmsHomePage,
-  }))
-);
-
-const HomePage = lazy(() =>
-  import("../pages/external/HomePage").then((module) => ({
-    default: module.HomePage,
-  }))
-);
-const NotFoundPage = lazy(() =>
-  import("../pages/external/NotFoundPage").then((module) => ({
-    default: module.NotFoundPage,
-  }))
-);
+import { LoginPage } from "../pages/external/LoginPage";
+import { ForgotPasswordPage } from "../pages/external/ForgotPasswordPage";
+import { CmsHomePage } from "../pages/internal/CmsHomePage";
+import { HomePage } from "../pages/external/HomePage";
+import { NotFoundPage } from "../pages/external/NotFoundPage"; 
 
 export const Router = () => {
   return (
