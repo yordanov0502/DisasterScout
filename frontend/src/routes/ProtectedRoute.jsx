38c79728fd1d5ed2006608f7ced865fe397ac379
yordanov0502 <yordanovtodor281@gmail.com>
2024-03-12 15:42:01 +0200

    ProtectedRoute updated

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,14 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useUserContext } from "../hooks/useUserContext";
 import { useQuery } from "@tanstack/react-query";
 import { isUserAuthenticated } from "../services/userService";
-import { useRef } from "react";
+import { useEffect, } from "react";
 
 const LOCAL_STORAGE_KEY1 = `${import.meta.env.VITE_LOCAL_STORAGE_KEY1}`;
 const LOCAL_STORAGE_VALUE1 = `${import.meta.env.VITE_LOCAL_STORAGE_VALUE1}`;
 
 export const ProtectedRoute = ({ children }) => {
   const { isUserContextEmpty,updateUserContext } = useUserContext();
-  const hasUpdatedUserContext = useRef(false);
 
   const getUserAuthentication = useQuery({
     queryKey: ["isUserAuthenticated"],
@@ -17,36 +16,27 @@ export const ProtectedRoute = ({ children }) => {
     enabled: isUserContextEmpty(), //!The query executes ONLY if the userContext is empty
   });
 
-  function doWork(data) {
-    if(!hasUpdatedUserContext.current){
-      updateUserContext(data);
-      hasUpdatedUserContext.current = true; // Set the flag to true after updating
+  useEffect(() => {
+     if (localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
+         localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
+         isUserContextEmpty() && 
+         getUserAuthentication.isSuccess) {
+           
+      updateUserContext(getUserAuthentication.data.data);
+      
     }
-    //console.log("updated user context");
-    console.log(data);
-    return children;
-  }
+  }, [getUserAuthentication.isSuccess]);
 
-  return(
-    <>
-         {
-          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
-          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
-          !isUserContextEmpty() && children
-         }
-         {
-          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
-          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
-          isUserContextEmpty() && getUserAuthentication.isLoading && <h1>Loading</h1> 
-         }
-         {
-          localStorage.getItem(LOCAL_STORAGE_KEY1) !== null &&
-          localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 &&
-          isUserContextEmpty() && getUserAuthentication.isSuccess &&  doWork(getUserAuthentication.data)
-         }
-         {
-           localStorage.getItem(LOCAL_STORAGE_KEY1) === null && <Navigate to="/login" />
-         }
-    </>
-  );
+  if(localStorage.getItem(LOCAL_STORAGE_KEY1) !== null && localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 && !isUserContextEmpty())
+    {
+      return children;
+    }
+
+  if(localStorage.getItem(LOCAL_STORAGE_KEY1) !== null && localStorage.getItem(LOCAL_STORAGE_KEY1) === LOCAL_STORAGE_VALUE1 && isUserContextEmpty())
+    {
+      if(getUserAuthentication.isLoading) return <h1> Loading</h1>
+      if(getUserAuthentication.isSuccess) return children;
+    }
+
+    return <Navigate to="/login" />;
 };
